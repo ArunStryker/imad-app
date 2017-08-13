68c7c09b1ad01c198e0a537329d6e831d6079138
Arun Venkatesh <30900529+ArunStryker@users.noreply.github.com>
2017-08-13 17:58:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,1 @@
 console.log('Loaded!');
-
-document.getElementById('me');
-me.ontoggleclick = function() {
-    me.style.marginLeft='200px';
-    me.style.transform = 'scale(1.5)';
-    me.style.transition = 'all 0.5s ease-in-out';
-};
