64f4840b663c0cd689262f481239b517c7f9a32e
Arun Venkatesh <30900529+ArunStryker@users.noreply.github.com>
2017-08-13 17:29:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 
 document.getElementById('me');
 me.onhover = function() {
-    me.style.width= '200px';
-    me.style.height = '100px';
+    me.style.width= '500px';
+    me.style.height = '300px';
 };
