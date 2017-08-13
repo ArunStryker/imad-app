39732cd4e5d227e5d7f6657a340404bad81a9f4e
Arun Venkatesh <30900529+ArunStryker@users.noreply.github.com>
2017-08-13 17:42:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 
 document.getElementById('me');
 me.onhover = function() {
-    me.style.transform = scale(1.5);
-    transition = 'all 0.5s ease-in-out';
+    me.style.transform = 'scale(1.5)';
+    me.style.transition = 'all 0.5s ease-in-out';
 };
